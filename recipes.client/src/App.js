77b441d94c2662f6fe811f74201b7985f080f1ac
zomgra1 <hiban.com@gmail.com>
2023-03-09 14:49:17 +0100

    recipeService added

--- recipes.client/src/App.js
+++ recipes.client/src/App.js
@@ -1,9 +1,23 @@
+import { useEffect, useState } from 'react';
 import {Route, Router} from 'react-router-dom'
 import './App.css';
+import { getAllRecipes } from './services/recipeServise';
 
 function App() {
+
+  const [recipesInfo, setRecipesInfo] = useState([]);
+
+  useEffect(()=>{
+    async function getRecipes(){
+      await setRecipesInfo(await getAllInfoRecipes());
+    }
+    getRecipes();
+},[])
+
+
   return (
     <div className="App">
+       <SearchBar />
       <Router>
         <Route element={<Front/>} path='/'></Route>
       </Router>
---
+++ recipes.client/src/services/recipeServise.jsx
@@ -0,0 +1,41 @@
+import axios from 'axios';
+import { DEFAULT_API_URL } from '../utils/Constants';
+
+const instance = axios.create({
+    baseURL: DEFAULT_API_URL,
+});
+
+export function getAllInfoRecipes() {
+    var jsonRes;
+
+    instance.get('/recipe/small',{
+        method:"GET"
+    }).then(responce=>responce.json()).then(json=> jsonRes = json)
+    .catch(e=> console.error(e))
+
+    console.log(jsonRes);
+    return jsonRes;
+}
+
+export function getAllRecipes() {
+    var jsonRes;
+
+    instance.get('/recipe',{
+        method:"GET"
+    }).then(responce=>responce.json()).then(json=> jsonRes = json)
+    .catch(e=> console.error(e))
+
+    console.log(jsonRes);
+    return jsonRes;
+}
+export function getAllIngredients() {
+    var jsonRes;
+
+    instance.get('/ingredient',{
+        method:"GET"
+    }).then(responce=>responce.json()).then(json=> jsonRes = json)
+    .catch(e=> console.error(e))
+
+    console.log(jsonRes);
+    return jsonRes;
+}
