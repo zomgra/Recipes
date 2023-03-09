c1dbb1fd1bf861ed76778f2ddc8c55aefcac72c1
zomgra1 <hiban.com@gmail.com>
2023-03-09 14:53:00 +0100

    added new Service

--- recipes.client/src/ui/Search/SearchBar.jsx
+++ recipes.client/src/ui/Search/SearchBar.jsx
@@ -5,19 +5,20 @@ export default function SearchBar({ingredients}) {
     const [searchIngreadients, setSearchIngreadients] = useState([]);
     const [query, setQuery] = useState(null)
 
-    function searchIngredient(query) {
+    function seacrhIngredient(query) {
         setQuery(query);
-        setSearchIngreadients(ingredients.filter(ingredient => {
+        let searchIngredients = ingredients.filter(ingredient => {
             ingredient.name.ToLowerCase().include(query.ToLowerCase())
-        }));
+        });
+        if(seacrhIngredients) setSearchIngreadients(seacrhIngredients);
     }
 
   return (
     <div>
-        <input placeholder='Enter ingredient name' onChange={(e)=> {searchIngredient(e.target.value)}}/>
+        <input placeholder='Enter ingredient name' onChange={(e)=> {seacrhIngredient(e.target.value)}}/>
         {(query !== null && query.length !== 0) && (    
             <div>
-                {searchIngreadients.map(ingredient => {
+                {seacrhIngredients.map(ingredient => {
                     <div key={ingredient.id}>
                         <p>{ingredient.Name}</p>
                     </div>
