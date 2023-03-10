918758c4abff7a973322bf9224f01155f56312de
zomgra1 <hiban.com@gmail.com>
2023-03-10 22:13:51 +0100

    create RecipeInfo

--- recipes.client/src/ui/Recipe/RecipeInfo.jsx
+++ recipes.client/src/ui/Recipe/RecipeInfo.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
+import { getRecipeById } from '../../services/recipeServise';
 
 export default function RecipeInfo() {
-    const { id } = useParams(); 
-  return (
-    <div>RecipeInfo : {id}</div>
-  )
+    const { id } = useParams();
+    const [recipe, setRecipe] = useState();
+    useEffect(() => {
+        async function getRecipe() {
+            let recipe = await getRecipeById(id);
+            console.log(recipe);
+            setRecipe(recipe);
+        }
+        getRecipe();
+    }, [])
+    return (
+        <div>
+            <div className="jumbotron jumbotron-fluid bg-secondary">
+                <div className="container">
+                    <h1 class="text-center text-white display-1">{recipe.name}</h1>
+                </div>
+            </div>
+
+            <div className="container my-5">
+                <div className="row">
+                    <div className="col-md-6">
+                        <img src={recipe.imageUrl} alt={recipe.imageUrl} className="img-fluid"/>
+                    </div>
+                    <div className="col-md-6">
+                        <h2 className="mb-4">Лучшее блюдо Украины</h2>
+                        <p><strong>Описание:</strong></p>
+                        <p>{recipe.description}</p>
+                        <p><strong>Инструкция:</strong></p>
+                        <ul>
+                            <li>{recipe.instruction}</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
